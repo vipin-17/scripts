c3a44b1ca2655aa04c3367771d327e584ebb23b5
vipin-17 <88742117+vipin-17@users.noreply.github.com>
2021-08-16 21:57:44 +0530

    Create terminate-0days.sh

---
+++ terminate-0days.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Script to terminate users
+
+function sbterminate() { /etc/seedbox/sbmanager/venv/bin/python /etc/seedbox/sbmanager/user.py --json del $1 --days=0; }
+
+read -p "Submit the list of Usernames for termination: `echo $'\n>'`" input
+
+for i in ${input[@]}
+do
+    USERN=$(ls /etc/seedbox/user/ | grep "$i")
+    if [[ "$USERN" == "$i" ]]; then
+    echo "User for termination:" $i
+    sbterminate $i
+    
+    else
+     echo "USER NOT VALID"
+    
+    fi
+
+done
