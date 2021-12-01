0844f15723787fb7204f916dabdf74b2ff8043a0
vipin-17 <88742117+vipin-17@users.noreply.github.com>
2021-12-01 11:49:04 +0530

    Update traffic-fix.sh

--- traffic-fix.sh
+++ traffic-fix.sh
@@ -2,26 +2,26 @@
 # Script to Adjust Traffic as requested
 
 #Set the Traffic amount first
-read -p "Traffic Amount: `echo $`" input
-TRAFFIC_AMNT="'$1'000511627776"
+read -p "Traffic Amount: " input
+TRAFFIC_AMNT=""$input"000511627776"
 
-echo "Checked Entered Traffic" $TRAFFIC_AMNT
+echo "Entered Traffic in bytes" $TRAFFIC_AMNT
 
+#Proceed with Adjustment now
+function TRAFFIC_FIX() { /etc/seedbox/sbmanager/venv/bin/python /etc/seedbox/sbmanager/user.py modify --max-traffic=$TRAFFIC_AMNT $1; }
 
-#function TRAFFIC_FIX() { /etc/seedbox/sbmanager/venv/bin/python /etc/seedbox/sbmanager/user.py modify --max-traffic=$1 $2; }
+echo
+read -p "Enter the Usernames for adjustment now: `echo $'\n>'`" input
 
-#read -p "Enter the Usernames with  Traffic Amount for adjustment: `echo $'\n>'`" input
+for i in ${input[@]}
+do
+    USERN=$(ls /etc/seedbox/user/ | grep "$i")
+    if [[ "$USERN" == "$i" ]]; then
+    TRAFFIC_FIX $i >/dev/null 2>&1
+    echo "[ADJUSTMENT DONE]:"  $i $TRAFFIC_AMNT >> /root/traffic-adjustment.txt
 
-#for i in ${input[@]}
-#do
-#    USERN=$(ls /etc/seedbox/user/ | grep "$i")
-#    if [[ "$USERN" == "$i" ]]; then
- #   echo "User for suspension:" $i
-  #  sbsuspend $i >/dev/null 2>&1
-#   echo "[SUSPENDED]:"  $i
+    else
+     echo "[USER NOT VALID]:" $i
+    fi
 
- #   else
-  #   echo "[USER NOT VALID]:" $i
-   # fi
-
-#done
+done
