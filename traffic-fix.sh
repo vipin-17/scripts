26edf169f1a48db22cc43c3d0af73bccec892b52
vipin-17 <88742117+vipin-17@users.noreply.github.com>
2021-12-01 11:20:33 +0530

    Update traffic-fix.sh

--- traffic-fix.sh
+++ traffic-fix.sh
@@ -8,21 +8,20 @@ TRAFFIC_AMNT="'$1'000511627776"
 echo "Checked Entered Traffic" $TRAFFIC_AMNT
 
 
-function TRAFFIC_FIX() { /etc/seedbox/sbmanager/venv/bin/python /etc/seedbox/sbmanager/user.py modify --max-traffic=$1 $2; }
+#function TRAFFIC_FIX() { /etc/seedbox/sbmanager/venv/bin/python /etc/seedbox/sbmanager/user.py modify --max-traffic=$1 $2; }
 
-read -p "Enter the Usernames with  Traffic Amount for adjustment: `echo $'\n>'`" input
+#read -p "Enter the Usernames with  Traffic Amount for adjustment: `echo $'\n>'`" input
 
-for i in ${input[@]}
-do
-    USERN=$(ls /etc/seedbox/user/ | grep "$i")
-    if [[ "$USERN" == "$i" ]]; then
-    echo "User for suspension:" $i
-    sbsuspend $i >/dev/null 2>&1
+#for i in ${input[@]}
+#do
+#    USERN=$(ls /etc/seedbox/user/ | grep "$i")
+#    if [[ "$USERN" == "$i" ]]; then
+ #   echo "User for suspension:" $i
+  #  sbsuspend $i >/dev/null 2>&1
+#   echo "[SUSPENDED]:"  $i
 
-    echo "[SUSPENDED]:"  $i
+ #   else
+  #   echo "[USER NOT VALID]:" $i
+   # fi
 
-    else
-     echo "[USER NOT VALID]:" $i
-    fi
-
-done
+#done
