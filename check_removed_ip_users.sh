b4d28614c9fb12bcdf52f80d0774738d89e51799
vipin-17 <88742117+vipin-17@users.noreply.github.com>
2022-09-01 07:36:41 +0530

    Update check_removed_ip_users.sh

--- check_removed_ip_users.sh
+++ check_removed_ip_users.sh
@@ -6,7 +6,7 @@ REMOVED_IPS=$(grep "REMOVED_IPS" /etc/seedbox/sbmanager/settings/local_settings.
 # get users list of removed IP
 for ip in $REMOVED_IPS
 do
-    /etc/seedbox/sbmanager/venv/bin/python /etc/seedbox/sbmanager/manager.py srvinfo | grep $ip
+    /etc/seedbox/sbmanager/venv/bin/python /etc/seedbox/sbmanager/manager.py srvinfo | grep $ip 2>&1 > /dev/null
     result=$?
     if [[ "$result" != "0" ]]; then
         echo "    $ip was removed from the pool"
