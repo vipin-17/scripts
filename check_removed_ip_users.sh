75fa83466163c2a8a1a34412ec02eb7a90913e83
vipin-17 <88742117+vipin-17@users.noreply.github.com>
2022-09-01 06:53:06 +0530

    Create check_removed_ip_users.sh

---
+++ check_removed_ip_users.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#get removed IPs from local_settings.py
+REMOVED_IPS=$(grep "REMOVED_IPS" /etc/seedbox/sbmanager/settings/local_settings.py | grep -Eo '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}')
+
+# get users list of removed IP
+for ip in $REMOVED_IPS
+do
+    /etc/seedbox/sbmanager/venv/bin/python /etc/seedbox/sbmanager/manager.py srvinfo | grep $ip
+    result=$?
+    if [[ "$result" != "0" ]]; then
+        echo "    [ALL GOOD] $ip not assigned to any user!"
+    else
+        echo "    [NOT GOOD] $ip still assigned to some users!"
+done
