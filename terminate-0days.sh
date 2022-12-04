e3b1a46992ff92f78ab1a0087ded2d12f5f0572f
vipin-17 <88742117+vipin-17@users.noreply.github.com>
2022-12-04 10:53:31 +0530

    Update terminate-0days.sh

--- terminate-0days.sh
+++ terminate-0days.sh
@@ -9,13 +9,12 @@ for i in ${input[@]}
 do
     USERN=$(ls /etc/seedbox/user/ | grep "$i")
     if [[ "$USERN" == "$i" ]]; then
-        if [[ id -u "$USERN" ]]; then
-            echo "User for termination:" $i
+        if id -u "$USERN" 2>&1 > /dev/null; then
+            printf "[+] User for termination: $i ...\n\n"
             sbterminate $i
-    
         else
-            echo "USER NOT VALID"
-        fi    
+            printf "[-] USER NOT VALID !!!\n\n"
+        fi
     fi
 
 done
